6ab41e5b61fc8b52d3af2582c5c2a6b7760f4f33
ChickenCoupe <158774862+ChickenCoupe@users.noreply.github.com>
2024-03-02 12:12:20 -0500

    Update road_seg.py

--- ROS/src/road_seg.py
+++ ROS/src/road_seg.py
@@ -9,18 +9,18 @@ from cv_bridge import CvBridge as bridge
 import time
 
 
-FILEPATH = "enet-model.net" #fill with Local filepath to model parameters
-CLASSES = "class.txt" #fill with Local filepath to class params
-COLORS = "color.txt" #fill with Local filepath to color params (used for object vs. road vs. undetected)
-VIS = "vis.txt"
+FILEPATH = "/home/cev/catkin_ws/src/road_seg/src/enet-model.net" #fill with Local filepath to model parameters
+CLASSES = open("/home/cev/catkin_ws/src/road_seg/src/class.txt","r") #fill with Local filepath to class params
+colors = open("/home/cev/catkin_ws/src/road_seg/src/color.txt","r").read().strip().split("\n") #fill with Local filepath to color params (used for object vs. road vs. undetected)
+name = "/home/cev/catkin_ws/src/road_seg/src/vis.txt"
+VIS = open(name, "r")
 vis = VIS.read().strip().split("\n")
-print(vis)
-vis = [np.array(c.split(",")).astype("int") for c in COLORS]
-vis = np.array(COLORS, dtype="uint8")
+vis = [np.array(c.split(",")).astype("int") for c in colors]
+vis = np.array(colors, dtype="uint8")
 
 classes = CLASSES.read().strip().split("\n")
-colors = open(COLORS).read().strip().split("\n")
 net = cv2.dnn.readNet(FILEPATH)
+print("predicted")
 legend = np.zeros(((len(classes) * 25) + 25, 300, 3), dtype="uint8")
 for (i, (className, color)) in enumerate(zip(classes, colors)):
 	# draw the class name + color on the legend
@@ -29,13 +29,16 @@ for (i, (className, color)) in enumerate(zip(classes, colors)):
 		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 	cv2.rectangle(legend, (100, (i * 25)), (300, (i * 25) + 25),
 		tuple(color), -1)
+print("color setup")
 
 def seg_callback(data):
+    print("called")
     image = bridge.imgmsg_to_cv2(data, desired_encoding = 'passthrough')
     image = imutils.resize(image, width=500)
     blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (1024, 512), 0,
 	swapRB=True, crop=False)
     net.setInput(blob)
+    print("set input")
     start = time.time()
     output = net.forward()
     end = time.time()
@@ -55,9 +58,13 @@ def seg_callback(data):
 if __name__ == '__main__':
     while 1:
         try:
+            print(1)
             rospy.init_node('RSeg', anonymous=True)
+            print(2)
             pub = rospy.Publisher('RSeg', Bool, queue_size=1)
+            print(3)
             depthsub = rospy.Subscriber("/zed/zed_node/depth/depth_registered", Image, seg_callback)
+            print(4)
             rospy.spin()
         except rospy.ROSInterruptException:
             pass
